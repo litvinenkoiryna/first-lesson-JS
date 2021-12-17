87e2ade2d700eb552563bc9217f520b03c35caa0
litvinenkoiryna <litvinenkoiryna@gmail.com>
2021-12-17 19:38:17 +0200

    first commit

---
+++ my_hw/homework_01.js
@@ -0,0 +1,42 @@
+let userName;
+let $nameSurname;
+let _userNameSurname;
+let user123;
+
+//let 125;
+//let Namesurname;
+//let name-surname;
+//let .124rt56gfd;
+//let ФИО;
+//let a;
+//let let; нельзя зарезервированное слово
+
+
+//коментування коду з однієї строки
+
+/*коментування коду
+з декількох
+строк  */
+
+//стили  написания
+//1
+let userName = 'Iryna';
+let age = 34;
+let city = 'Kramatorsk';
+
+//2
+let userName = 'Iryna', age = 34, city = 'Kramatorsk';
+
+//3
+let userName = 'Iryna',
+    age = 34,
+    city = 'Kramatorsk';
+
+
+
+
+
+
+
+
+
---
+++ my_hw/homework_02.js
@@ -0,0 +1,21 @@
+//number 1
+let username = prompt('What is your name?');
+
+alert(`Hello, ${username}!`);
+
+//number 2
+const year = 2021;
+let age = prompt ('Какого ты года?');
+let message = (`${year-age}`);
+alert(`${message}`);
+
+//number 3
+let length = prompt('Назви довжину сторони квадрата')
+alert (`${length *4}`)
+
+
+
+
+
+
+
